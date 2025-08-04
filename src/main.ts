e67f999ef8d02ac56ad26edf37183f1bbc6775e0
419425903@qq.com <419425903@qq.com>
2025-08-04 17:18:32 +0800

    feat: 🚀 新增了element-plus的图标库

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { createApp } from 'vue';
-import './style.css';
 import App from './App.vue';
 
-//element-plus
+//引入element-ui
 import ElementPlus from 'element-plus';
+//引入element ui的样式
 import 'element-plus/dist/index.css';
 
 const app = createApp(App);
